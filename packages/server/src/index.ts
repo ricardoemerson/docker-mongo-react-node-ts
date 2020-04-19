de669727c1f1e420145c5b08f081254cca470f68
Lucas Bittencourt <lucasgdbittencourt@gmail.com>
2020-04-19 19:07:56 -0300

    add: Express interface

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,11 +1,11 @@
-import express, { Request, Response } from 'express';
+import express, { Express, Request, Response } from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import dotenv from 'dotenv'
 
 dotenv.config();
 
-const app = express();
+const app: Express = express();
 const { MONGO_URL, PORT } = process.env;
 
 app.use(cors());
@@ -16,22 +16,16 @@ app.get('/api/v1/data', (req: Request, res: Response) =>
     res.status(200).json({ response: 'hello-world' })
 );
 
-async function main() {
-    try {
-        await mongoose.connect(MONGO_URL, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-            useFindAndModify: false,
-            useCreateIndex: true,
-        });
+mongoose.connect(MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+}, err => {
+    if (err) throw err;
 
-        app.listen(PORT, () =>
-            // tslint:disable-next-line: no-console
-            console.log('The server has been started.')
-        );
-    } catch (err) {
-        throw err;
-    }
-}
-
-main();
+    app.listen(PORT, () =>
+        // tslint:disable-next-line: no-console
+        console.log('The server has been started.')
+    );
+})
