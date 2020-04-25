dde69928cc269bd8c5331b763ed3f76649605c5d
Lucas Bittencourt <lucasgdbittencourt@gmail.com>
2020-04-25 18:27:23 -0300

    update: serverAPI.ts

--- packages/web/src/services/serverAPI.ts
+++ packages/web/src/services/serverAPI.ts
@@ -3,24 +3,24 @@ import axios from 'axios';
 import configs from '~/configs';
 import { IAuth } from './authentication';
 
-const { REACT_APP_PUBLIC_URL } = configs;
 const auth = localStorage.getItem('R:auth');
+const { REACT_APP_PUBLIC_URL } = configs;
 
-let serverAPI = axios.create({
-	baseURL: `${REACT_APP_PUBLIC_URL}/api/v1`,
-});
+let accessToken;
 
 if (auth) {
 	const parseAuth: IAuth = JSON.parse(auth);
 
 	if (parseAuth.authenticated) {
-		serverAPI = axios.create({
-			baseURL: `${REACT_APP_PUBLIC_URL}/api/v1`,
-			headers: {
-				Authorization: `Bearer ${parseAuth.accessToken}`,
-			},
-		});
+		accessToken = parseAuth.accessToken;
 	}
 }
+
+const serverAPI = axios.create({
+	baseURL: `${REACT_APP_PUBLIC_URL}/api/v1`,
+	headers: {
+		Authorization: `Bearer ${accessToken}`,
+	},
+});
 
 export default serverAPI;
